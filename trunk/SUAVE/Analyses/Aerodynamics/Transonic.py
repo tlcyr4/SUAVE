d6583de26b2b6a6dd1b7834166b6acd3092986dd
Tigar Cyr <tlcyr4@aol.com>
2017-06-30 12:31:52 -0400

    Adds hybrid single variable model

--- trunk/SUAVE/Analyses/Aerodynamics/Transonic.py
+++ trunk/SUAVE/Analyses/Aerodynamics/Transonic.py
@@ -19,6 +19,10 @@ class Transonic(Aerodynamics):
         Model based on NASA CRM Transonic Experimental Data
     """
     
+    def __defaults__(self):
+        self.fzero = Fidelity_Zero.Fidelity_Zero()
+        
+    
     
     def evaluate(self, state):
         # lift model
@@ -44,55 +48,68 @@ class Transonic(Aerodynamics):
         beta = state.conditions.aerodynamics.side_slip_angle
         phi = [180] + state.conditions.aerodynamics.roll_angle
         
-        if mach[0,0] < .75:
-            model = Fidelity_Zero.Fidelity_Zero()
-            #model.settings = self.settings
-            model.geometry = self.geometry
-            
-            model.process.compute.lift.inviscid_wings.geometry = self.geometry
-            model.process.compute.lift.inviscid_wings.initialize()
-            
-            return model(state)
+        first = True
         
-        # prepare vector
-        predictors = np.copy(mach)
-        predictors = np.append(predictors, alpha, 1)
-        predictors = np.append(predictors, beta, 1)
-        predictors = np.append(predictors, phi, 1)
+        if mach[0,0] < .8:
+            if first:
+                self.fzero.geometry = self.geometry
+                self.fzero.process.compute.lift.inviscid_wings.geometry = self.geometry
+                self.fzero.process.compute.lift.inviscid_wings.initialize()
+                first = False
+            return self.fzero(state)
+        else:
         
-        predictors = np.transpose(predictors)
+            # prepare vector
+            predictors = np.copy(mach)
+            predictors = np.append(predictors, alpha, 1)
+            predictors = np.append(predictors, beta, 1)
+            predictors = np.append(predictors, phi, 1)
+        
+            predictors = np.transpose(predictors)
+        
+            # cl, cd = quadratic_model(predictors)
+            cl, cd = quartic_aoa(np.transpose(alpha))
         
         
-        
-        # model coefficients
-        bl = np.array([[-435.789730005553], [0.742983792057789], [-0.421988197961623], [0.129666568157724], [-0.00512396188693175], [8.11823341193399], [-68.8488118397378], [1.96751918027884], [0.00250750492442697]])
-        bd = np.array([[8.57222709094397], [-0.897800934092333], [0.626871359450883], [1.78975587167322e-05], [0.00131719217498857], [-7.33801361840798], [119.803398627759], [0.144319023849905], [-0.00105278583037119]])
-        
-        # calculate
-        cl = bl[0] + np.dot(np.transpose(bl[1::2]), predictors) + np.dot(np.transpose(bl[2::2]), np.square(predictors))
-        cd = bd[0] + np.dot(np.transpose(bd[1::2]), predictors) + np.dot(np.transpose(bd[2::2]), np.square(predictors))
-        
-        def printall(i):
-            print 'predictors'
-            print predictors[:,i]
-            print 'cl'
-            print cl[0,i]
-            print 'cd'
-            print cd[0,i]
-        
-        """for i in range(len(cl[0])):
+            """for i in range(len(cl[0])):
             if cl[0,i] < 0 or cd[0,i] < 0:
                 printall(i)
             if predictors[1,i] > 10:
                 printall(i)
              """   
+            # Pack up results
+            results.drag.total = np.transpose(cd)
+            results.lift.total = np.transpose(cl)
         
-        # Pack up results
-        results.drag.total = np.transpose(cd)
-        results.lift.total = np.transpose(cl)
         
+            return results
+    
+def quadratic_model(predictors):
         
-        return results
+    # model coefficients
+    bl = np.array([[-435.789730005553], [0.742983792057789], [-0.421988197961623], [0.129666568157724], [-0.00512396188693175], [8.11823341193399], [-68.8488118397378], [1.96751918027884], [0.00250750492442697]])
+    bd = np.array([[8.57222709094397], [-0.897800934092333], [0.626871359450883], [1.78975587167322e-05], [0.00131719217498857], [-7.33801361840798], [119.803398627759], [0.144319023849905], [-0.00105278583037119]])
+            
+    # calculate
+    cl = bl[0] + np.dot(np.transpose(bl[1::2]), predictors) + np.dot(np.transpose(bl[2::2]), np.square(predictors))
+    cd = bd[0] + np.dot(np.transpose(bd[1::2]), predictors) + np.dot(np.transpose(bd[2::2]), np.square(predictors))
+            
+    return cl, cd
+def quartic_aoa(aoa):
+    bd = [[.0016], [.00044316],[.0141]]
+    bl = [[.000058828], [-.0015], [.00027147], [.1452], [.0980]]
+    cd = bd[2] + bd[1] * aoa + bd[0] * np.power(aoa,2)
+    if aoa[0,3] > 10:
+        print cd[0,3]
+    cl = bl[4] + bl[3] * aoa + bl[2] * np.power(aoa,2) + bl[1] * np.power(aoa,3) + bl[0] * np.power(aoa,4)
+    return cl, cd
+def printall(i):
+    print 'predictors'
+    print predictors[:,i]
+    print 'cl'
+    print cl[0,i]
+    print 'cd'
+    print cd[0,i]
 # ----------------------------------------------------------------------
 #   Module Testing
 # ----------------------------------------------------------------------   

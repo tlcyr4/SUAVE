2e4e9cb336c8d2b13c851ac108ff0e46727b8cbf
Tigar Cyr <tlcyr4@aol.com>
2017-07-07 11:38:22 -0400

    adds tandem model, documentation, code cleanup. streamlines non-transonic, removes unused models

--- trunk/SUAVE/Analyses/Aerodynamics/Transonic.py
+++ trunk/SUAVE/Analyses/Aerodynamics/Transonic.py
@@ -10,32 +10,48 @@ import SUAVE.Analyses.Aerodynamics.Aerodynamics as Aerodynamics
 import SUAVE.Analyses.Aerodynamics.Results as Results
 import numpy as np
 import Fidelity_Zero
+import Supersonic_Zero
     
 # ----------------------------------------------------------------------
 #   Example
 # ----------------------------------------------------------------------   \
 class Transonic(Aerodynamics):
     """ SUAVE.Analyses.Aerodynamics.Transonic()
-        Model based on NASA CRM Transonic Experimental Data
+        Model based on NASA CRM Transonic Experimental Data (curve fits)
     """
     
     def __defaults__(self):
-        self.fzero = Fidelity_Zero.Fidelity_Zero()
+        """ SUAVE.Analyses.Aerodynamics.Transonic.__defaults__()
+            initializes non-transonic models
+        """
+        # initialize subsonic model
+        model = Fidelity_Zero.Fidelity_Zero()
+        model.geometry = self.geometry    
+        model.process.compute.lift.inviscid_wings.geometry = self.geometry
+        model.process.compute.lift.inviscid_wings.initialize()
+        self.fzero = model
+        
+        # initialize supersonic model
+        model = Supersonic_Zero.Supersonic_Zero()
+        model.geometry = self.geometry
+        model.process.compute.lift.inviscid_wings.geometry = self.geometry
+        model.process.compute.lift.inviscid_wings.initialize()
+        self.szero = Supersonic_Zero.Supersonic_Zero()
         
     
     
     def evaluate(self, state):
-        print state.conditions.frames.body.thrust_force_vector[:,0]
-        # lift model
-        # y ~ b1 + b2*x1 + b3*x1^2 + b4*x2 + b5*x2^2 + b6*x3 + b7*x3^2 + b8*x4 + b9*x4^2
-        # b = [-435.789730005553 0.742983792057789 -0.421988197961623 0.129666568157724 -0.00512396188693175 8.11823341193399 -68.8488118397378 1.96751918027884 0.00250750492442697]
-        # R^2 = .991
-
-        # drag model
-        # y ~ b1 + b2*x1 + b3*x1^2 + b4*x2 + b5*x2^2 + b6*x3 + b7*x3^2 + b8*x4 + b9*x4^2
-        # b = [8.57222709094397 -0.897800934092333 0.626871359450883 1.78975587167322e-05 0.00131719217498857 -7.33801361840798 119.803398627759 0.144319023849905 -0.00105278583037119]
-        # R^2 = .947
-        
+        """ SUAVE.Analyses.Aerodynamics.evaluate(state)
+            evaluate aerodynamic analysis
+            
+            Inputs:
+                state.conditions.freestream.mach_number - mach number (column array)
+                state.conditions.aerodynamics.angle_of_attack - aoa (column array)
+                
+            Outputs:
+                results.lift.total - lift coefficient (column array)
+                results.drag.total - drag coefficient (column array)
+        """
         
         # Initialize results object
         results = Results()
@@ -45,74 +61,67 @@ class Transonic(Aerodynamics):
         # Unpack input
         mach = state.conditions.freestream.mach_number
         alpha = state.conditions.aerodynamics.angle_of_attack
-        beta = state.conditions.aerodynamics.side_slip_angle
-        phi = [180] + state.conditions.aerodynamics.roll_angle
         
-        if mach[0,0] < .8:
-            model = Fidelity_Zero.Fidelity_Zero()
-            #model.settings = self.settings
-            model.geometry = self.geometry
+        
+        mean = np.mean(mach)
+        # Subsonic model
+        if mean < .8:
+            results = self.fzero(state)
+
+        # Supersonic model
+        elif mean > 1.2:
+            results = self.szero(state)
             
-            model.process.compute.lift.inviscid_wings.geometry = self.geometry
-            model.process.compute.lift.inviscid_wings.initialize()
+        else:
             
-            results = model(state)
+            cl, cd = tandem(alpha)
             
-            return results
-        
-        # prepare vector
-        predictors = np.copy(mach)
-        predictors = np.append(predictors, alpha, 1)
-        predictors = np.append(predictors, beta, 1)
-        predictors = np.append(predictors, phi, 1)
-        
-        cl, cd = quadratic_single(alpha)
-        
-        results.lift.total = cl
-        results.drag.total = cd
-        
-        state.conditions.aerodynamics.lift_coefficient = cl
-        state.conditions.aerodynamics.drag_coefficient = cd
-        
+            results.lift.total = cl
+            results.drag.total = cd
+            
         return results
     
-def quadratic_multi(predictors):
-    
-    # model coefficients
-    bl = np.array([[-435.789730005553], [0.742983792057789], [-0.421988197961623], [0.129666568157724], [-0.00512396188693175], [8.11823341193399], [-68.8488118397378], [1.96751918027884], [0.00250750492442697]])
-    bd = np.array([[8.57222709094397], [-0.897800934092333], [0.626871359450883], [1.78975587167322e-05], [0.00131719217498857], [-7.33801361840798], [119.803398627759], [0.144319023849905], [-0.00105278583037119]])
-    
-    # calculate
-    cl = bl[0] + np.dot(np.transpose(bl[1::2]), predictors) + np.dot(np.transpose(bl[2::2]), np.square(predictors))
-    cd = bd[0] + np.dot(np.transpose(bd[1::2]), predictors) + np.dot(np.transpose(bd[2::2]), np.square(predictors))
-    
-    return cl, cd
-        
 def quadratic_single(aoa):
+    """ SUAVE.Analyses.Aerodynamics.Transonic.tandem(aoa)
+        Uses aoa to calculate cl and cd with quadratic curve fits
+        
+        Inputs:
+            aoa - angle of attack (column array)
+            
+        Outputs:
+            cl - lift coefficient (column array)
+            cd - drag coefficient (column array)
+    """
     # model coefficients
     bl = np.array([[-0.00717433179381432],	[0.144047701950893],	[0.126479444126983]])
     bd = np.array([[0.00162236444781382],	[0.000443156656073951],	[0.0141465405789619]])
+    
+    # calculate coefficients
     cl = bl[2] + bl[1] * aoa + bl[0] * np.square(aoa)
     cd = bd[2] + bd[1] * aoa + bd[0] * np.square(aoa)
     
-    
+    return cl, cd
 
+def tandem(aoa):
+    """ SUAVE.Analyses.Aerodynamics.Transonic.tandem(aoa)
+        Uses aoa to calculate cl and cl to calculate cd
+        
+        Inputs:
+            aoa - angle of attack (column array)
+            
+        Outputs:
+            cl - lift coefficient (column array)
+            cd - drag coefficient (column array)
+    """
+    # model coefficients
+    bl = np.array([[-0.00717433179381432],	[0.144047701950893],	[0.126479444126983]])
+    bd = np.array([[0.177990108978307],	[0.04257289502394],	[0.010275150311500]])
+    
+    # calculate coefficients
+    cl = bl[2] + bl[1] * aoa + bl[0] * np.square(aoa)
+    cd = bd[2] + bd[1] * cl + bd[0] * np.square(cl)
+    
     return cl, cd
-def quartic_aoa(aoa):
-    bd = [[.0016], [.00044316],[.0141]]
-    bl = [[.000058828], [-.0015], [.00027147], [.1452], [.0980]]
-    cd = bd[2] + bd[1] * aoa + bd[0] * np.power(aoa,2)
-    if aoa[0,3] > 10:
-        print cd[0,3]
-    cl = bl[4] + bl[3] * aoa + bl[2] * np.power(aoa,2) + bl[1] * np.power(aoa,3) + bl[0] * np.power(aoa,4)
-    return cl, cd
-def printall(predictors,cl, cd, i):
-    print 'predictors'
-    print predictors[:,i]
-    print 'cl'
-    print cl[0,i]
-    print 'cd'
-    print cd[0,i]
 # ----------------------------------------------------------------------
 #   Module Testing
 # ----------------------------------------------------------------------   
@@ -129,10 +138,10 @@ def main():
     state.conditions.aerodynamics.roll_angle = np.array([[180.0]])
     
     transonic = Transonic()
-    result = transonic.evaluate(state)
-    #print result.drag.total
-    #print result.lift.total
-    print transonic.settings.maximum_lift_coefficient
+#    result = transonic.evaluate(state)
+#    print result.drag.total
+#    print result.lift.total
+#    print transonic.settings.maximum_lift_coefficient
     
 #    model = Fidelity_Zero.Fidelity_Zero()
 #    model.settings = trans.settings
